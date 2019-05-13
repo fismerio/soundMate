5d2bc0994da1bc906bcf0d516f7d7aba35520bdf
fismerio <fismerio@gmail.com>
2019-05-13 17:02:32 -0300

    first image

---
+++ soundmate.sh
@@ -0,0 +1,6 @@
+echo "# soundMate" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/fismerio/soundMate.git
+git push -u origin master
