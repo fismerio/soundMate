6ffa392b289e3514aca0b8c728b7a08a3e32bf54
fismerio <fismerio@users.noreply.github.com>
2019-05-14 07:46:15 -0300

    Shell script
    Modify shell script soundmate.sh so path does not point to my repository

--- soundmate.sh
+++ soundmate.sh
@@ -2,5 +2,5 @@ echo "# soundMate" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
-git remote add origin https://github.com/fismerio/soundMate.git
+#git remote add origin https://github.com/ yourGitHubUserName /soundMate.git
 git push -u origin master
